cf7bcd2af45dd8dec7d4de113c9add825957aeae
lokkingdown-a11y <lokkingdown@gmail.com>
2026-06-24 22:02:24 +0600

    Update day1.py

--- day1.py
+++ day1.py
@@ -1,97 +1,97 @@
-# Task 1
-# name = input('Enter Name: ')
-# age = int(input('Enter Age: '))
-# city = input('Enter City: ')
-# print('Name','=',name,'age','=',age,'City','=',city)
-# Task 2
-# num1 = int(input('Enter Number: '))
-# num2 = int(input('Enter Number: '))
-# print(num1 + num2)
-# Task3 
-# age = int(input('Enter Age: '))
-# if age >= 18:
-#     print('You Are Adult')
-# else:
-#     print('Child')
-# Task4 
-# fruit_list = ['apple','orange','banana','jackfruit','watermelon']
-# print(fruit_list[0])
-# print(fruit_list[-1])
-# Task5
-# for i in range(1,6):
-#     print(i)
-# Task6
-# i = 10
-# while i >= 1:
-#     print(i)
-#     i += -1
-# Task 7
-# num = int(input('Enter Number: '))
-# if num > 0:
-#     print('The Number Is Positive')
-# elif num == 0:
-#     #print('The Number Is 0')
-# else:
-#     print('The Number Is Negative')
-# Task 8
-# Year = int(input('Enter Year: '))
-# if Year % 400 == 0 or Year % 4 ==0:
-#     print('The Year is LeapYear')
-# else:
-#     print('The Year Is not LeapYear')
-# Task 9
-# List_of_num = []
-# List_of_num.append(int(input('Enter Number: ')))
-# List_of_num.append(int(input('Enter Number: ')))
-# List_of_num.append(int(input('Enter Number: ')))
-# List_of_num.append(int(input('Enter Number: ')))
-# List_of_num.append(int(input('Enter Number: ')))
-# print(List_of_num)
-# Task 10
-# for i in range(2,52,2):
-#     print(i)
-# Task 11
-# n = int(input('Enter Number: '))
-# i = 1
-# while i <= 10:
-#     print(n,'*',i,'=',n*i)
-#     i = i + 1
-# Task12
-# password = input('Enter Password: ')
-# while password != 'secret123':
-#     print('finding......')
-#     password = input('Enter Password again: ')
-# print('Founded')
-# Task 13
-# name = input('Enter name: ')
-# V_list = ['a','e','i','o','u']
-# for char in name.lower():
-#     if char in V_list:
-#         print(char)
-# Task 14
-# list_nums = []
-# list_nums.append(int(input('Enter Number: ')))
-# list_nums.append(int(input('Enter Number: ')))
-# list_nums.append(int(input('Enter Number: ')))
-# list_nums.append(int(input('Enter Number: ')))
-# list_nums.append(int(input('Enter Number: ')))
-# largest = list_nums[0]
-# i = 0
-# while i < len(list_nums):
-#     if list_nums[i] > largest:
-#         largest = list_nums[i]
-#     i += 1
-# print(largest)
-# Task 15
-# n = int(input('Enter Number: ')) 
-# m = '*' 
-# for i in range(1,n+1):
-#     print(m*i)
-# Task 16
-# List_Nums = [3,3,8,5,7,3,12,3,4,54,6,7,9]
-# for num in List_Nums:
-#     if num % 2 == 0:
-#         continue
-# #     print(num)
+Task 1
+name = input('Enter Name: ')
+age = int(input('Enter Age: '))
+city = input('Enter City: ')
+print('Name','=',name,'age','=',age,'City','=',city)
+Task 2
+num1 = int(input('Enter Number: '))
+num2 = int(input('Enter Number: '))
+print(num1 + num2)
+Task3 
+age = int(input('Enter Age: '))
+if age >= 18:
+    print('You Are Adult')
+else:
+    print('Child')
+Task4 
+fruit_list = ['apple','orange','banana','jackfruit','watermelon']
+print(fruit_list[0])
+print(fruit_list[-1])
+Task5
+for i in range(1,6):
+    print(i)
+Task6
+i = 10
+while i >= 1:
+    print(i)
+    i += -1
+Task 7
+num = int(input('Enter Number: '))
+if num > 0:
+    print('The Number Is Positive')
+elif num == 0:
+    #print('The Number Is 0')
+else:
+    print('The Number Is Negative')
+Task 8
+Year = int(input('Enter Year: '))
+if Year % 400 == 0 or Year % 4 ==0:
+    print('The Year is LeapYear')
+else:
+    print('The Year Is not LeapYear')
+Task 9
+List_of_num = []
+List_of_num.append(int(input('Enter Number: ')))
+List_of_num.append(int(input('Enter Number: ')))
+List_of_num.append(int(input('Enter Number: ')))
+List_of_num.append(int(input('Enter Number: ')))
+List_of_num.append(int(input('Enter Number: ')))
+print(List_of_num)
+Task 10
+for i in range(2,52,2):
+    print(i)
+Task 11
+n = int(input('Enter Number: '))
+i = 1
+while i <= 10:
+    print(n,'*',i,'=',n*i)
+    i = i + 1
+Task12
+password = input('Enter Password: ')
+while password != 'secret123':
+    print('finding......')
+    password = input('Enter Password again: ')
+print('Founded')
+Task 13
+name = input('Enter name: ')
+V_list = ['a','e','i','o','u']
+for char in name.lower():
+    if char in V_list:
+        print(char)
+Task 14
+list_nums = []
+list_nums.append(int(input('Enter Number: ')))
+list_nums.append(int(input('Enter Number: ')))
+list_nums.append(int(input('Enter Number: ')))
+list_nums.append(int(input('Enter Number: ')))
+list_nums.append(int(input('Enter Number: ')))
+largest = list_nums[0]
+i = 0
+while i < len(list_nums):
+    if list_nums[i] > largest:
+        largest = list_nums[i]
+    i += 1
+print(largest)
+Task 15
+n = int(input('Enter Number: ')) 
+m = '*' 
+for i in range(1,n+1):
+    print(m*i)
+Task 16
+List_Nums = [3,3,8,5,7,3,12,3,4,54,6,7,9]
+for num in List_Nums:
+    if num % 2 == 0:
+        continue
+#     print(num)
 
         
